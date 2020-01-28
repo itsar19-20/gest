8ae0e911d574ae4d6d56383da64f853e228da34b
Luca <Luca@LAPTOP-CKROPSCC>
2020-01-28 17:37:04 +0100

    Aggiunti metodi per avere le scadenze nel mese desiderato (fattura in entrata, in uscita o entrambe) Più metodo per la notifica di avviso della scadenza(provvisorio ma funzionante).

--- code/Stonks/src/main/java/business/Scadenziario.java
+++ code/Stonks/src/main/java/business/Scadenziario.java
@@ -25,27 +25,9 @@ public class Scadenziario {
 	
 	private int anticipoNotifica;
 	
-	
-	
-	
-	
-	
 	public Scadenziario() {
 		
-		
-		
-		
-		
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	
 	public static void showEntrataDaConcludere(Persona pers) {
 		List<Pagamento> entrataDaConcludere=JPALuke.selectParziale(pers,true);
@@ -55,13 +37,7 @@ public class Scadenziario {
 					"\nSaldo Dovuto: "+p.getFattura().getLordo());
 		
 		}
-		
-		
-		
-		
 	}
-
-	
 
 	public static void  showUscitaDaConcludere(Persona pers) {
 		List<Pagamento> entrataDaConcludere=JPALuke.selectParziale(pers,false);
@@ -71,21 +47,22 @@ public class Scadenziario {
 					"\nSaldo Dovuto: "+p.getFattura().getLordo());
 		
 		}
-		
+	}
+	
+	public static void showMeseUscita(Persona pers, Integer mese) {
+		List<Pagamento> entrataDaConcludere=JPALuke.selectParziale(pers,false);
+		Scadenziario.searchByMese(mese, entrataDaConcludere);
+	}
+	
+	public static void showMeseEntrata(Persona pers,Integer mese) {
+		List<Pagamento> entrataDaConcludere=JPALuke.selectParziale(pers,true);
+		Scadenziario.searchByMese(mese, entrataDaConcludere);
 		
 	}
 	
-	//   
-	public static void mesePagamento(Integer mese) {
-		
-	}
-	
-	
-
 	public int getAnticipoNotifica() {
 		return 5;
 	}
-
 
 	public void setAnticipoNotifica(int anticipoNotifica) {
 		
@@ -102,55 +79,66 @@ public class Scadenziario {
 		}
 	}
 	
-	public void showScadenziarioMese(Persona pers,Integer mese) {
+	public static void showScadenziarioMese(Persona pers,Integer mese) {
 		//select tutti i pagamenti dell'utnte non ancora completati
 				List<Pagamento> scadenzaMese=JPALuke.selectPagamenti(pers);
-				Date dataCorrente=new Date();
-				Calendar cal=Calendar.getInstance(TimeZone.getTimeZone("Europe/Rome"), Locale.ITALY);
-				cal.setTime(dataCorrente);
+				Scadenziario.searchByMese(mese, scadenzaMese);
 				
-				// ricerca in base a N mesi successivi a quello corrente delle scadenze fatture
-				for(Pagamento pag:scadenzaMese) {
-					Date dataFattura=pag.getFattura().getData();
-					int scadenza=pag.getFattura().getScadenza();
-					Calendar c=Calendar.getInstance(TimeZone.getTimeZone("Europe/Rome"), Locale.ITALY);
-					c.setTime(dataFattura);
-					
-					Calendar d=(Calendar) cal.clone();
-					c.add(Calendar.DAY_OF_YEAR,+scadenza );
-					d.add(Calendar.MONTH , +mese);
-					
-					if(c.get(Calendar.MONTH)==d.get(Calendar.MONTH)) {
-						System.out.println("id: "+pag.getIdPagamento()+"\nData Fattura: "+pag.getFattura().getData()+
-								"\nScadenza fattura: "+ pag.getFattura().getScadenza());
-					}
-					
-					
-					
-					
-				}
-				
-				
-				
-				
-				
-				
-				
-				
-				
+	}
+	
+	private static void searchByMese(Integer mese, List<Pagamento> scadenzaMese) {
+		Date dataCorrente=new Date();
+		Calendar cal=Calendar.getInstance(TimeZone.getTimeZone("Europe/Rome"), Locale.ITALY);
+		cal.setTime(dataCorrente);
 		
+		// ricerca in base a N mesi successivi a quello corrente delle scadenze fatture
+		for(Pagamento pag:scadenzaMese) {
+			
+			Calendar c=Calendar.getInstance(TimeZone.getTimeZone("Europe/Rome"), Locale.ITALY);
+			c.setTime(pag.getFattura().getData());
+			
+			Calendar d=(Calendar) cal.clone();
+			c.add(Calendar.DAY_OF_YEAR,+pag.getFattura().getScadenza() );
+			d.add(Calendar.MONTH , +mese);
+			
+			if(c.get(Calendar.MONTH)==d.get(Calendar.MONTH)) {
+				System.out.println("id: "+pag.getIdPagamento()+"\nData Fattura: "+pag.getFattura().getData()+
+						"\nScadenza fattura: "+ pag.getFattura().getScadenza());
+			}
+		}
+	}
+	
+	private static void searchForNotifica(List<Pagamento> pagamentiDaConcludere) {
+
+		Date dataOggi=new Date();
+		Calendar cal=Calendar.getInstance(TimeZone.getTimeZone("Europe/Rome"), Locale.ITALY);
+		cal.setTime(dataOggi);
 		
+		for(Pagamento pag:pagamentiDaConcludere) {
+			Calendar calFattura=Calendar.getInstance(TimeZone.getTimeZone("Europe/Rome"), Locale.ITALY);
+			
+			calFattura.setTime(pag.getFattura().getData());
+			Calendar calCorrente=(Calendar) cal.clone();
+			
+			// l'anticipo notifica va memorizzato nel local Storage, per ora uso 5
+			calCorrente.add(Calendar.DAY_OF_YEAR, +5 );
+			calFattura.add(Calendar.DAY_OF_YEAR, +pag.getFattura().getScadenza());
+			if(calCorrente.getTime().after(calFattura.getTime())) {
+				System.out.println("La fattura di id: "+pag.getFattura().getIdFattura()+" sta per scadere");
+			}	
+		}
 	}
 	
 	
-	
-	
-
-	
-	public void checkLoginNotifica() {
-			
+	public static void checkNotifica(Persona pers) {
+		List<Pagamento> pagamentiDaConcludere=JPALuke.selectPagamenti(pers);
+		Scadenziario.searchForNotifica(pagamentiDaConcludere);	
 		}
-		
+	
+		public static void checkNotifica(Persona pers ,boolean entrataUscita) {
+			List<Pagamento> listaPagamento=JPALuke.selectParziale(pers,entrataUscita);
+			Scadenziario.searchForNotifica(listaPagamento);			
+		}
 	}
 
 
