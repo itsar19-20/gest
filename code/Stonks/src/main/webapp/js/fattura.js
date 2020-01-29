f86f06ddd089e7d3ae737a6a6c40753e5e6e65a3
Albi99 <alberto.sormani.070@gmail.com>
2020-01-29 18:06:41 +0100

    albi - ...

--- code/Stonks/src/main/webapp/js/fattura.js
+++ code/Stonks/src/main/webapp/js/fattura.js
@@ -1,17 +1,87 @@
 $(() => {
 
+    //  button guarda fatture
+    $('#btn-guarda-fatture').click(function() {
+        var btn = 'button guarda fatture '
+        console.log(btn, '.click');
+        $.ajax({
+            url: '/fattura/guarda',
+            method: 'get'
+        })
+        .done(function(listaFatture) {
+            console.log(btn, '.done', listaFatture);
+            
+            /*
+            listaFatture.forEach(element => {
+                $('#ul-fatture').append(`<li>ID: ${element.id}</li>`);              
+                console.log('ho aggiunto un list item');
+            });
+            */
+
+            /*
+            // Unordered List
+            $('#ul-fatture').empty();
+            listaFatture.forEach(element => {
+                $('#ul-fatture').append(`<li>ID: ${element.id}</li>`);
+            });
+            console.log('ho terminato la UL');
+            */
+
+            //  DataTable
+            $('#tbl-fatture').DataTable({
+                data: listaFatture,
+                columns: [
+                    {title: 'ID', data: 'id'},
+                    {title: 'Data', data: 'data'},
+                    {title: 'Scadenza', data: 'scadenza'},
+                    {title: 'Fattura cliente', data: 'fatturaCliente'},
+                    {title: 'Persona', data: 'persona'},
+                    {title: 'Nota', data: 'nota'},
+                    {title: 'Conto', data: 'conto'},
+                    {title: 'IVA', data: 'iva'},
+                    {title: 'Lordo', data: 'lordo'},
+                ]
+            });
+            
+
+        })
+        .fail(function() {
+            console.log(btn, '.fail');
+        })
+        .always(function() {
+            console.log(btn, '.always', listaFatture);
+        });
+        console.log(btn, '.EndClick');
+    });
+
     //  load the first articol
     $.ajax({
         url: '/parts/articolo.html',
         method: 'get'
     })
     .done((html) => {
-        $('#articoli').html(html);
-    })
+        var numeroArticoli = 'add-new';
+        $('#ol-articoli').append(html.replace('§numero§', numeroArticoli).replace('§numero§', numeroArticoli).replace('§btn-text§', '+'));
+        numeroArticoli = 1;
+        console.log(numeroArticoli);
 
-    // add another articol
-    $('#btn-add-articol').click(() => {
-        
+        // add another articol
+        $('#btn-articol-add-new').click(() => {
+            numeroArticoli++;
+            console.log(numeroArticoli)
+
+            $.ajax({
+                url: '/parts/articolo.html',
+                method: 'get'
+            })
+            .done((html) => {
+                var rimuoviDiv = $('#articoli').remove( $('#articolo-list-item-' + `${numeroArticoli}`) );
+                $('#ol-articoli').append(html.replace('§numero§', numeroArticoli).replace('§numero§', numeroArticoli).replace('§btn-text§', '-').replace('§click§', 'alert(\'pippo\', \`${rimuoviDiv}\`);'));
+
+                // remove an articol
+
+            });
+        });
     });
 
 });
