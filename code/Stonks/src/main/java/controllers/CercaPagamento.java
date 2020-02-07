896f964f2f05c29161d64b0c0e00c4dc603f2765
Luca <Luca@192.168.203.183>
2020-02-07 09:01:37 +0100

    aggiungo cercaPagamento

---
+++ code/Stonks/src/main/java/controllers/CercaPagamento.java
@@ -0,0 +1,60 @@
+package controllers;
+
+import java.io.IOException;
+
+import javax.persistence.EntityManager;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import models.Pagamento;
+import utils.JPALuke;
+import utils.JPAUtil;
+
+@WebServlet("/cercaPagamento")
+public class CercaPagamento extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+    public CercaPagamento() {
+        super();
+        
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		System.out.println("ho raggiunto la servlet cercaPagamento");
+		Integer idFattura=Integer.parseInt(request.getParameter("fattura"));
+		System.out.println("Ho ricevuto la fattura di id: "+idFattura);
+		EntityManager emTemp=JPAUtil.getInstance().getEmf().createEntityManager();
+		Pagamento pagamento=JPALuke.searchPagamento(idFattura,emTemp);
+		
+		
+		
+		if(pagamento!=null) {
+		System.out.println("il gia pagato è: "+pagamento.getGiaPagato());
+		request.getSession().removeAttribute("pagamento");
+		request.getSession().setAttribute("pagamento", pagamento);
+		ObjectMapper om = new ObjectMapper();
+		response.setContentType("application/json");
+		response.getWriter().append(om.writeValueAsString(pagamento));
+		emTemp.close();
+		}else {
+			request.getSession().setAttribute("pagamento", null);
+			request.getSession().removeAttribute("fatturaId");
+			request.getSession().setAttribute("fatturaId", idFattura);
+		}
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request, response);
+	}
+
+}
