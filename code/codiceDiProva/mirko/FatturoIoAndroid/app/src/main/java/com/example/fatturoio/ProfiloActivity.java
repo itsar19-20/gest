2cee22c316ab8b7230b46c88e0200a949d7eae03
CptNMitchell <m.rondi@itsrizzoli.it>
2020-02-07 10:49:04 +0100

    inizio progetto android.

---
+++ code/codiceDiProva/mirko/FatturoIoAndroid/app/src/main/java/com/example/fatturoio/ProfiloActivity.java
@@ -0,0 +1,13 @@
+package com.example.fatturoio;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProfiloActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
