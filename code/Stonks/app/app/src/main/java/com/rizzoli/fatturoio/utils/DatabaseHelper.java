0eb3b031335a9b0e3696c8399f8740b4536a277d
Albi99 <alberto.sormani.070@gmail.com>
2020-03-12 17:46:05 +0100

    modifiche DatabaseHelper.java (non completo)

--- code/Stonks/app/app/src/main/java/com/rizzoli/fatturoio/utils/DatabaseHelper.java
+++ code/Stonks/app/app/src/main/java/com/rizzoli/fatturoio/utils/DatabaseHelper.java
@@ -12,17 +12,38 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     private static final String
             DATABASE_NAME = "localDatabase.db",
 
-            CREATE_TABLE_FATTURA = "CREATE TABLE fattura (" +
-                    "_id integer primary key autoincrement, " +
-                    "note text not null," +
-                    "lordo float not null)",
+            CREATE_TABLE_FATTURA = "CREATE TABLE fattura ("
+            		+ "_id integer primary key autoincrement, "
+            		+ "data text not null, "
+            		+ "scadenza integer not null, "
+            		// + "e_una_fattura_cliente boolean not null, "
+            		+ "persona integer not null, "
+            		+ "numero_fattura text not null, "
+            		+ "iva float not null, "
+            		+ "lordo float not null, "
+            		// + "pagata boolean not null, "
+            		+ "conto integer not null",
             CREATE_TABLE_ARTICOLO = "CREATE TABLE articolo ("
             		+ "_id integer primary key autoincrement, "
-            		+ "descrizione text no"
-            		+ "quantita",
-            CREATE_TABLE_PERSONA = "",
-            CREATE_TABLE_USER = "CREATE TABLE user ( " +
-                    "_id integer primary key autoincrement )";
+            		+ "descrizione text not null, "
+            		+ "quantita float not null, "
+            		+ "prezzo float not null, "
+            		+ "parziale float not null, "
+            		+ "fattura integer not null)",
+            CREATE_TABLE_PERSONA = "CREATE TABLE persona ("
+            		+ "_id integer primary key autoincrement, "
+            		+ "nome text not null, "
+            		+ "cognome text not null, "
+            		+ "pIVA text not null, "
+            		+ "mail text not null, "
+            		+ "indirizzo text not null, "
+            		+ "telefono text not null, "
+            		+ "autore integer not null)",
+            CREATE_TABLE_USER = "CREATE TABLE user ( "
+                    + "_id integer primary key autoincrement,"
+                    + "username text not null, "
+                    + "password text not null,"
+                    + "dataOraUltimoLogin text not null)";
 
     public DatabaseHelper(@Nullable Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -31,12 +52,16 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(CREATE_TABLE_FATTURA);
+        db.execSQL(CREATE_TABLE_ARTICOLO);
+        db.execSQL(CREATE_TABLE_PERSONA);
         db.execSQL(CREATE_TABLE_USER);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS fattura");
+        db.execSQL("DROP TABLE IF EXISTS articolo");
+        db.execSQL("DROP TABLE IF EXISTS persona");
         db.execSQL("DROP TABLE IF EXISTS user");
         onCreate(db);
     }
